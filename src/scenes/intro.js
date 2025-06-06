a5853f8b258badbc54c876b7f6fc028471f12553
Aceep <alycia.gautier@laposte.net>
2025-06-06 21:01:34 +0200

    structure

--- src/scenes/intro.js
+++ src/scenes/intro.js
@@ -1,20 +1,24 @@
+import { runDialogue } from './dialogue.js';
+
 export function runIntro() {
   return new Promise((resolve) => {
     const introVideo = document.getElementById('introVideo');
     const videoElem = document.getElementById('videoElem');
     const blackScreen = document.getElementById('blackScreen');
 
-    videoElem.addEventListener('ended', () => {
+    videoElem.addEventListener('ended', async () => {
       introVideo.classList.remove('visible');
+      blackScreen.classList.add('visible');
 
-      setTimeout(() => {
-        blackScreen.classList.add('visible');
-      }, 1000);
-
-      setTimeout(() => {
+      setTimeout(async () => {
         blackScreen.classList.remove('visible');
-        resolve();
-      }, 2500);
+        setTimeout(async () => {
+          await runDialogue('owlIntro');
+          resolve();
+        }, 500);
+      }, 1500);
     });
   });
 }
+
+
