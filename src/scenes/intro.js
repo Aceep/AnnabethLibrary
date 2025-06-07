dd03bb6cdaf02ccb43f9158851084b58423fb369
Aceep <alycia.gautier@laposte.net>
2025-06-07 17:23:58 +0200

    feat: backgrounds and dialogues

--- src/scenes/intro.js
+++ src/scenes/intro.js
@@ -1,23 +1,9 @@
 import { runDialogue } from './dialogue.js';
 
 export function runIntro() {
-  return new Promise((resolve) => {
-    const introVideo = document.getElementById('introVideo');
-    const videoElem = document.getElementById('videoElem');
-    const blackScreen = document.getElementById('blackScreen');
-
-    videoElem.addEventListener('ended', async () => {
-      introVideo.classList.remove('visible');
-      blackScreen.classList.add('visible');
-
-      setTimeout(async () => {
-        blackScreen.classList.remove('visible');
-        setTimeout(async () => {
-          await runDialogue('owlIntro');
-          resolve();
-        }, 500);
-      }, 1500);
-    });
+  return new Promise(async (resolve) => {
+    await runDialogue('intro');
+    await runDialogue('owlIntro');
   });
 }
 
